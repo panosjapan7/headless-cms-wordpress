8868fc6003cd726dae29922578f2d6046c407531
panosjapan7 <panosjapan@gmail.com>
2022-09-07 09:12:41 +0200

    Gives class to anchor instead of a tag

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import "../styles/Navbar.css"
+import React from "react";
+import "../styles/Navbar.css";
 
 function NavBar() {
   return (
-        <div className="navbar">
-            <div className="nav-wrapper">
-              <div className="logo">
-                  <a href="/"><i className="fa fa-angellist"></i>WEBB21</a>
-              </div>
-              <div id="menu">
-                  <a href="/">Home</a>
-                  <a href="/about">About</a>
-              </div>
-          </div>
+    <div className="navbar">
+      <div className="nav-wrapper">
+        <div className="logo">
+          <a className="anchor" href="/">
+            <i className="fa fa-angellist"></i>WEBB21
+          </a>
+        </div>
+        <div id="menu">
+          <a className="anchor" href="/">
+            Home
+          </a>
+          <a className="anchor" href="/about">
+            About
+          </a>
+        </div>
       </div>
-  )
+    </div>
+  );
 }
 
 export default NavBar;
