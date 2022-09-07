7c2e38ca5ecc543d5a1b5fa311ab63e527dc8192
panosjapan7 <panosjapan@gmail.com>
2022-09-07 09:14:55 +0200

    formatting

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,37 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import "../styles/Home.css"
+import "../styles/Home.css";
 
 function Home() {
-
   const [posts, setPosts] = useState([]);
 
   const fetchData = async () => {
-    const response = await fetch(`${process.env.REACT_APP_API_URL}/posts`)
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/posts`);
     const data = await response.json();
     setPosts(data.posts);
-    console.log(data)
-  }
+    console.log(data);
+  };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-    return (
-        <div className="home">
-            {posts &&
-                posts.map((post) => {
-                    return (
-                        <div className="post" key={post.ID}>
-                            <h1 className="post-title">{post.title}</h1>
-                            <Link to={`/posts/${post.ID}`}>
-                                <p className="read-more">Read more..</p>
-                            </Link>
-                            <hr></hr>
-                        </div>
-                    );
-                })}
-        </div>
-    );
+  return (
+    <div className="home">
+      {posts &&
+        posts.map((post) => {
+          return (
+            <div className="post" key={post.ID}>
+              <h1 className="post-title">{post.title}</h1>
+              <Link to={`/posts/${post.ID}`}>
+                <p className="read-more">Read more..</p>
+              </Link>
+              <hr></hr>
+            </div>
+          );
+        })}
+    </div>
+  );
 }
 
 export default Home;
