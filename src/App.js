4f02e579a09e0f3236551593931453ee77b3b83b
panosjapan7 <panosjapan@gmail.com>
2022-09-07 09:13:41 +0200

    Adds CategoryList, Route to Category

--- src/App.js
+++ src/App.js
@@ -3,24 +3,24 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Post from "./pages/Post";
+import Category from "./pages/Category";
 import NavBar from "./components/NavBar";
+import CategoryList from "./components/CategoryList";
 
 function App() {
-
   return (
     <Router>
-
-        <NavBar />
+      <NavBar />
+      <CategoryList />
       <>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/posts/:id" element={<Post />} />
+          <Route path="/:category" element={<Category />} />
         </Routes>
       </>
     </Router>
-
-
   );
 }
 
